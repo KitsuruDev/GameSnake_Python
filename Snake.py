6756c73bb20feb69ef1c0a1c91ab0564da9c3082
KitsuruDev <kitsubasedev@gmail.com>
2025-04-03 14:54:21 +0300

    Добавлены все файлы Змейки в ветку main

--- Snake.py
+++ Snake.py
@@ -1,7 +1,6 @@
-from tkinter import Tk, Canvas, ALL, Event
+from tkinter import Tk, Canvas, ALL
 from tkinter.ttk import Button, Style
 from random import randint
-import unittest
 
 class SnakeGame:
 
@@ -34,7 +33,7 @@ class SnakeGame:
         self.buttonDifMid = Button(text="Средняя", command = lambda: self.__start('Mid'))
         self.buttonDifHig = Button(text="Высокая", command = lambda: self.__start('Hig'))
 
-        self.form.bind("<KeyPress>", self.move_change)
+        self.form.bind("<KeyPress>", self.__move_change)
         self.form.bind("<KeyPress-Escape>", lambda i: self.form.quit())
 
         self.snake = [(20, 20)]
@@ -85,14 +84,14 @@ class SnakeGame:
         self.snake_orientation = 'Down'
         self.snake_speed = 150 if diff != 'Low' else 250
         if diff == 'Hig':
-            self.pos_stone = self.place_stone()
-        self.pos_food = self.place_food()
+            self.pos_stone = self.__place_stone()
+        self.pos_food = self.__place_food()
         self.score = 0
         self.game = True
 
         self.__run()
 
-    def place_stone(self):
+    def __place_stone(self):
         pos_list = []
         for i in range(randint(4, 6)):
             while True:
@@ -102,18 +101,18 @@ class SnakeGame:
                     break
         return pos_list
 
-    def place_food(self):
+    def __place_food(self):
         while True:
             pos = (randint(0, 19) * 20, randint(0, 19) * 20)
             if pos not in self.snake and pos not in self.pos_stone:
                 return pos
 
-    def move_change(self, event):
+    def __move_change(self, event):
         if event.keysym in ('Up', 'Down', 'Left', 'Right'):
             if SnakeGame._u_turn[event.keysym] != self.snake_orientation: # разворот на "180 градусов"
                 self.snake_orientation = event.keysym
 
-    def move(self):
+    def __move(self):
         direction_mapping = {
             'Up':    lambda x, y: (x, y - 20),
             'Down':  lambda x, y: (x, y + 20),
@@ -123,20 +122,17 @@ class SnakeGame:
         new_head = direction_mapping[self.snake_orientation](*self.snake[0])
         new_head = (new_head[0] % 400, new_head[1] % 400) # при выходе за границу
 
-        if new_head in self.pos_stone:
+        if new_head in self.snake or new_head in self.pos_stone:
             self.game = False
             return
 
+        self.snake.insert(0, new_head)
+
         if new_head == self.pos_food:
             self.score += 1
-            self.pos_food = self.place_food()
+            self.pos_food = self.__place_food()
         else:
             self.snake.pop()
-
-        if new_head in self.snake:
-            self.snake = [new_head]
-        else:
-            self.snake.insert(0, new_head)
 
     def __draw(self):
         self.canvas.delete(ALL)
@@ -157,7 +153,7 @@ class SnakeGame:
 
     def __run(self):
         if self.game:
-            self.move()
+            self.__move()
             self.__draw()
             self.form.after(self.snake_speed, self.__run) # миллисекунды
         else:
@@ -172,65 +168,7 @@ class SnakeGame:
             self.buttonGame.place(x=130, y=200)
             self.buttonReturn.place(x=110, y=250)
 
-
-class TestSnakeGame(unittest.TestCase):
-
-    def setUp(self):
-        self.form = Tk()
-        self.game = SnakeGame(self.form)
-
-    def tearDown(self):
-        self.form.destroy()
-
-    def test_initial_game(self):
-        self.assertEqual(self.game.snake, [(20, 20)], "Начальная позиция змейки должна быть на (20, 20)")
-        self.assertEqual(self.game.snake_orientation, 'Down', "Начальное направление змейки должно быть 'Down'")
-        self.assertEqual(self.game.score, 0, "Начальный счёт должен быть равен 0")
-        self.assertFalse(self.game.game, "Игра ещё не должна быть запущена")
-
-    def test_move_change(self):
-        event = Event()
-
-        event.keysym = 'Left'
-        self.game.move_change(event)
-        self.assertEqual(self.game.snake_orientation, 'Left', "Змейка должна поменять своё направление на 'Left'")
-
-        event.keysym = 'Right'
-        self.game.move_change(event)
-        self.assertEqual(self.game.snake_orientation, 'Left', "Змейка не может поменять свою ориентацию на 180 градусов - направление 'Right'")
-
-    def test_place_stone(self):
-        stone_position = self.game.place_stone()
-        self.assertNotIn(stone_position, self.game.snake, "Камень не поможет появиться в змее")
-        self.assertIsInstance(stone_position[0], tuple, "Позиция камня должна быть картежом")
-        self.assertEqual(len(stone_position[0]), 2, "Позиция камня должна состоять из 2 координат")
-
-    def test_place_food(self):
-        food_position = self.game.place_food()
-        self.assertNotIn(food_position, self.game.snake, "Еда не поможет появиться в змее")
-        self.assertIsInstance(food_position, tuple, "Позиция еды должна быть картежом")
-        self.assertEqual(len(food_position), 2, "Позиция еды должна состоять из 2 координат")
-
-    def test_move(self):
-        initial_snake = self.game.snake.copy()
-        self.game.move()
-        new_head = (initial_snake[0][0], initial_snake[0][1] + 20)
-        self.assertEqual(self.game.snake[0], new_head, "Змейка должна перемещаться на 20 пикселей")
-
-        self.game.snake = [(20, 20), (20, 40)]
-        self.game.snake_orientation = 'Down'
-        self.game.move()
-        self.assertFalse(self.game.game, "Игра не должна заканчиваться, когда змейка попадает в себя")
-
-    def test_score_increase(self):
-        self.game.pos_food = (20, 40)
-        self.game.snake = [(20, 20), (20, 40)]
-        self.game.move()
-        self.assertEqual(self.game.score, 1, "Счёт должен увеличиться на 1 после поедания еды")
-        self.assertNotIn(self.game.pos_food, self.game.snake, "Еда должна быть в другой позиции после съедания")
-
 if __name__ == "__main__":
-    unittest.main()
-    # formMain = Tk()
-    # obj = SnakeGame(formMain)
-    # formMain.mainloop()
+    formMain = Tk()
+    obj = SnakeGame(formMain)
+    formMain.mainloop()
